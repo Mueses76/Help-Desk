83f88f7c9f175302dba430818ec9c7436a011a07
Hernando Mueses <hernandomueses@esap.edu.co>
2021-10-05 14:05:44 -0500

    commit primeros pasos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path')
+const exphbs = require('express-handlebars');
 
 //Initializations
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(_dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir:,
+    extname:,
+}));
 
 //Midlewares
 
