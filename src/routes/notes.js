868bbef148f8e1e49338930ae70aa673a9e5aed5
Hernando Mueses <hernandomueses@esap.edu.co>
2021-10-05 17:21:57 -0500

    commit install nodemon

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes', (req, res) => {
+    res.send('Notes from database');
+});
+
+
+router.get('/', (req, res) => {
+    res.send('Index');
+});
+
+
 module.exports = router;
