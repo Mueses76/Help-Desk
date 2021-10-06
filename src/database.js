ca0187590a91d24173f271332cb691acd3ca9ca9
Hernando Mueses <hernandomueses@esap.edu.co>
2021-10-06 13:35:10 -0500

    commit solventado error MongoParseError: option usefindandmodify is not supported

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/notes-db-app', {
-    useCreateIndex: true,
+   // useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false,
-});
+    
+    useUnifiedTopology: true    
+})
 
-   .then(db => console.log('DB is connected'))
-   .catch(err => console.error(err));
+.then(db => console.log('DB is connected'))
+.catch(err => console.error(err));
